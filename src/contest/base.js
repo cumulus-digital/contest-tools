09efe7d488fcd5fbb2c2245827d87cac37a46827
Daniel Vena <deaconvena+git@gmail.com>
2019-11-06 13:03:28 -0500

    Support expireReplace in loyalty

--- src/contest/base.js
+++ src/contest/base.js
@@ -68,4 +68,24 @@
 
 	};
 
+	/**
+	 * jQuery function to replace a given object's text with a new string
+	 * if the current time exceeds a given timestamp
+	 * @param  {(Date|string)} ts      Expire timestamp to test against
+	 * @param  {string}        newText Text or HTML to replace with
+	 * @return {void}
+	 */
+	$.fn.expireReplace = function(ts, newText){
+		if (Object.prototype.toString.call(ts) !== '[object Date]') {
+			ts = new Date(Date.parse(ts));
+		}
+		if ( ! (ts instanceof Date) || isNaN(ts.getTime())) {
+			log('Supplied expireReplace timestamp is an invalid date.', ts, ts instanceof Date);
+			return;
+		}
+		if (Date.now() > ts) {
+			this.text(newText);
+		}
+	};
+
 }(jQuery, window));
