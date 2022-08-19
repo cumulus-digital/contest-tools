60c7cb462c0690e581654547c56e4e31a87b7ac5
Daniel Vena <deaconvena+github@gmail.com>
2022-08-19 13:18:08 -0400

    restructure loyalty contest script

--- src/contest/index.js
+++ src/contest/index.js
@@ -5,6 +5,61 @@ import Logger from 'Utils/Logger.js';
 	window._CMLS = window._CMLS || {};
 	const DOC = window.document;
 
+	/**
+	 * Installs Google Analytics only if we're not inside an iframe.
+	 * @param  {string} id GA Property ID
+	 */
+	window._CMLS.installGoogleAnalytics = function (id) {
+		if (!id) {
+			console.warn('No Google Analytics ID provided.');
+			return;
+		}
+		if (window.top !== window.self) {
+			console.log(
+				'Contest is embedded, not installing Google Analytics again.'
+			);
+			return;
+		}
+		const scr = DOC.createElement('script');
+		scr.async = 'async';
+		scr.scr =
+			'https://www.googletagmanager.com/gtag/js?id=' +
+			encodeURIComponent(id);
+		scr.onload = function () {
+			window.dataLayer = window.dataLayer || [];
+			function gtag() {
+				window.dataLayer.push(arguments);
+			}
+			gtag('js', new Date());
+			gtag('config', id, { cookie_prefix: 'cmls_contest' });
+		};
+		DOC.body.appendChild(scr);
+	};
+
+	/**
+	 * jQuery function to replace a given object's text with a new string
+	 * if the current time exceeds a given timestamp
+	 * @param  {(Date|string)} ts      Expire timestamp to test against
+	 * @param  {string}        newText Text or HTML to replace with
+	 * @return {void}
+	 */
+	$.fn.expireReplace = function (ts, newText) {
+		if (Object.prototype.toString.call(ts) !== '[object Date]') {
+			ts = new Date(Date.parse(ts));
+		}
+		if (!(ts instanceof Date) || isNaN(ts.getTime())) {
+			log(
+				'Supplied expireReplace timestamp is an invalid date.',
+				ts,
+				ts instanceof Date
+			);
+			return;
+		}
+		if (Date.now() > ts) {
+			this.text(newText);
+		}
+	};
+
 	$(() => {
 		// Install iframeResizer content script
 		$.getScript(
@@ -64,59 +119,4 @@ import Logger from 'Utils/Logger.js';
 		/*jsl:end */
 		/* jshint ignore:end */
 	});
-
-	/**
-	 * Installs Google Analytics only if we're not inside an iframe.
-	 * @param  {string} id GA Property ID
-	 */
-	window._CMLS.installGoogleAnalytics = (id) => {
-		if (!id) {
-			console.warn('No Google Analytics ID provided.');
-			return;
-		}
-		if (window.top !== window.self) {
-			console.log(
-				'Contest is embedded, not installing Google Analytics again.'
-			);
-			return;
-		}
-		const scr = DOC.createElement('script');
-		scr.async = 'async';
-		scr.scr =
-			'https://www.googletagmanager.com/gtag/js?id=' +
-			encodeURIComponent(id);
-		scr.onload = function () {
-			window.dataLayer = window.dataLayer || [];
-			function gtag() {
-				window.dataLayer.push(arguments);
-			}
-			gtag('js', new Date());
-			gtag('config', id, { cookie_prefix: 'cmls_contest' });
-		};
-		DOC.body.appendChild(scr);
-	};
-
-	/**
-	 * jQuery function to replace a given object's text with a new string
-	 * if the current time exceeds a given timestamp
-	 * @param  {(Date|string)} ts      Expire timestamp to test against
-	 * @param  {string}        newText Text or HTML to replace with
-	 * @return {void}
-	 */
-	$.fn.expireReplace = function (ts, newText) {
-		if (Object.prototype.toString.call(ts) !== '[object Date]') {
-			ts = new Date(Date.parse(ts));
-		}
-		if (!(ts instanceof Date) || isNaN(ts.getTime())) {
-			log(
-				'Supplied expireReplace timestamp is an invalid date.',
-				ts,
-				ts instanceof Date
-			);
-			return;
-		}
-		if (Date.now() > ts) {
-			this.text(newText);
-		}
-	};
 })(jQuery, window.self);
